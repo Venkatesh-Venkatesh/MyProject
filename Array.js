49269d4ec9820b7fc02ba14276e10912f567def1
venkatesh M <Venkatesh.Manokaran@techgenzi.com>
2023-01-26 18:15:57 +0530

    first commit

---
+++ Array.js
@@ -0,0 +1,21 @@
+//mixed array
+var array=[1,"ko",true,4.4];
+console.log(array);
+
+//array constructor
+var stringArray=new Array();
+var numericArray=new Array();
+var decimalArray=new Array();
+var bolleanArray=new Array();
+stringArray[0]="ert";
+numericArray[1]=22;
+decimalArray[2]=9.9;
+bolleanArray[3]=true;
+console.log(stringArray);
+console.log(numericArray);
+console.log(decimalArray);
+console.log(bolleanArray);
+
+//null and undefined 
+var my=null;
+console.log(my);
